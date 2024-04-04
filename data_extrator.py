64921e0aa7b26e6b6ca44305bc6bf4b115fcb1b1
MichalT <trzebuniak.michal@gmail.com>
2024-04-04 12:34:07 +0200

    Added slice_overlap parameter, class, and edge case checks

--- data_extrator.py
+++ data_extrator.py
@@ -1,13 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def extract_parameters(data, time_step_0, slice_count=5, slice_shift=50, alpha_value = 0.8, slice_size = 100, parameters_per_slice = 6, plot_results = False):
+class DataExtractor:
 
-    if plot_results:
-        # Plot whole dataset
-        plt.plot([i for i in range(len(data))], data)
-
-    def exponential_filter(time_step):
+    def exponential_filter(time_step, data, slice_size, alpha_value, plot_results, parameters_per_slice):
         filtered_data = np.zeros((slice_size,))
         for filtered_index, raw_data_index in enumerate(range(time_step - slice_size, time_step)):
             if filtered_index == 0:
@@ -22,14 +18,26 @@ def extract_parameters(data, time_step_0, slice_count=5, slice_shift=50, alpha_v
         return filtered_data[-parameters_per_slice:]
 
 
-    parameters = np.zeros((slice_count, parameters_per_slice))
-    for i in range(slice_count):
-        # Calculate last point of slice
-        time_step = time_step_0 - i*slice_shift
-        # Calculate last 6 points of filtered values
-        parameters[i] = exponential_filter(time_step)
+    def exponential_filter_parameters(data, time_step_0, slice_count=5, slice_overlap=50, alpha_value = 0.8, slice_size = 100, parameters_per_slice = 6, plot_results = False):
+        # Check if enough data points
+        slice_shift = slice_size - slice_overlap
+        if time_step_0 - (slice_count-1) * slice_shift - slice_size < 0:
+            raise ValueError("Dataset is too small to extract parameters.")
+        if time_step_0 > len(data) - 1:
+            raise ValueError("time_step_0 is too big for given dataset.")
 
-    if plot_results:
-        plt.show()
-    return parameters
+        if plot_results:
+            # Plot whole dataset
+            plt.plot([i for i in range(len(data))], data)
+
+        parameters = np.zeros((slice_count, parameters_per_slice))
+        for i in range(slice_count):
+            # Calculate last point of slice
+            time_step = time_step_0 - i*slice_shift
+            # Calculate last 'parameters_per_slice' points of filtered values
+            parameters[i] = DataExtractor.exponential_filter(time_step, data, slice_size, alpha_value, plot_results, parameters_per_slice)
+
+        if plot_results:
+            plt.show()
+        return parameters
     
