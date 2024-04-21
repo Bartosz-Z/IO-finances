dbdbf3bba43da99953e6658530a900d483bcd242
Bartosz Zbiegien <bartosz_zbiegien@wp.pl>
2024-04-21 14:08:13 +0200

    Add cache to mdd and exponential extractor

--- ExtractorModules/mdd_extractor.py
+++ ExtractorModules/mdd_extractor.py
@@ -1,12 +1,17 @@
 import numpy as np
 from data_extractor import DataExtractor
+from typing import Dict
 
 
 class MddExtractor:
     def __init__(self, main_extractor: DataExtractor):
         self.main_extractor: DataExtractor = main_extractor
+        self.__cache: Dict[int, np.ndarray] = {}
 
     def get_maximum_drawdowns(self, time_step_0):
+        if time_step_0 in self.__cache:
+            return self.__cache[time_step_0]
+
         mdds = np.zeros((self.main_extractor.slice_count,))
         slice_shift = self.main_extractor.slice_size - self.main_extractor.slice_overlap
         for slice_idx in range(self.main_extractor.slice_count):
@@ -18,4 +23,6 @@ class MddExtractor:
             else:
                 j = np.argmax(slice_data[:i])
             mdds[slice_idx] = (slice_data[j] - slice_data[i]) / slice_data[j]
+
+        self.__cache[time_step_0] = mdds
         return mdds
